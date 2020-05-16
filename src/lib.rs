4cda8528ccfa7741b3d3e83b3149af5b94236d0c
Mark Smith <smith.h.mark@gmail.com>
2020-05-16 18:00:10 -0400

    case insensitive search, 12-21

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,17 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+fn search_case_insenitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = vec![];
+    let query = query.to_lowercase();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
 #[derive(PartialEq, Debug)]
 pub struct Config {
     pub query: String,
@@ -44,14 +55,28 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape!";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape!";
+
+        assert_eq!(vec!["safe, fast, productive.", "Duct tape!"],
+                   search_case_insenitive(query, contents));
     }
 
     #[test]
