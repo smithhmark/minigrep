5061ebc82d716ce07e76a61fd31d9f70d39d31b3
Mark Smith <smith.h.mark@gmail.com>
2020-05-16 17:05:19 -0400

    extract guts, 12-14

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,33 +13,9 @@ fn main() {
     println!("Searching for:{}", config.query);
     println!("Searching in file:{}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error:{}", e);
         process::exit(1);
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)
-        .expect("Oh no, file problem!");
-
-    println!("Contents:\n{}", contents);
-
-    Ok(()) //indiomatic: means "this function called for side-effects"
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config {query, filename})
-    }
-}
