4af3ded2515d27ee46fcac36a784ce41c1d6f4be
Mark Smith <smith.h.mark@gmail.com>
2020-05-16 16:44:30 -0400

    struct constructor, 12-7

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_args(&args);
+    let config = Config::new(&args);
     println!("Searching for:{}", config.query);
     println!("Searching in file:{}", config.filename);
 
@@ -19,8 +19,10 @@ struct Config {
     filename: String,
 }
 
-fn parse_args(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config {query, filename}
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config {query, filename}
+    }
 }
