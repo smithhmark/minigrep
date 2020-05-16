64967840eba62dd6541b4513d06e9b026cd1c9b7
Mark Smith <smith.h.mark@gmail.com>
2020-05-16 16:37:02 -0400

    extract parse, listing 12-5

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,17 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_args(&args);
     println!("Searching for:{}", query);
     println!("Searching in file:{}", filename);
 
     let contents = fs::read_to_string(filename).expect("Oh no, file problem!");
 
     println!("Contents:\n{}", contents);
+}
+
+fn parse_args(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+    (query, filename)
 }
