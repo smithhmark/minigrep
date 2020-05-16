72ad5b2a2ca308628c0d4011bb90e52967bdf1cb
Mark Smith <smith.h.mark@gmail.com>
2020-05-16 16:41:50 -0400

    bundle config, listing 12-6

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,23 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_args(&args);
-    println!("Searching for:{}", query);
-    println!("Searching in file:{}", filename);
+    let config = parse_args(&args);
+    println!("Searching for:{}", config.query);
+    println!("Searching in file:{}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Oh no, file problem!");
+    let contents = fs::read_to_string(config.filename)
+        .expect("Oh no, file problem!");
 
     println!("Contents:\n{}", contents);
 }
 
-fn parse_args(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_args(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config {query, filename}
 }
