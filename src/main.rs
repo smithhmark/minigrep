27793e96d05cb96cc92c0c4d71bb066b6f62a88a
Mark Smith <smith.h.mark@gmail.com>
2020-05-16 16:33:32 -0400

    listing 12-4

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("Args:{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+    println!("Searching for:{}", query);
+    println!("Searching in file:{}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Oh no, file problem!");
+
+    println!("Contents:\n{}", contents);
 }
